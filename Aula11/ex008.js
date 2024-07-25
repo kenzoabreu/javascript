16803c41977f297a836f38ffb36b71c5b8eb5192
Eduardo <japabreu10@gmail.com>
2024-07-25 08:20:34 -0300

    comit

--- Aula11/ex008.js
+++ Aula11/ex008.js
@@ -1,2 +1,9 @@
-var vel = 60.5
-console.log (`A velocidade`)
+var vel = 60.0
+console.log (`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade máxima permitida`)
+} else {
+    console.log(`Parabéns amigo`)
+}
+
+
---
+++ Aula11/ex009.js
@@ -0,0 +1,8 @@
+var país = "Brazil"
+console.log(`VocÊ mora em ${país}`)
+
+if(país != 'Brasil'){
+    console.log (`Olá brasileiro, beleza?`)
+} else {
+    console.log(`Hi gringo`)
+}
