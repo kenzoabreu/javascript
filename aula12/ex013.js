8c48a277529e350904d7cb15ebdc775b29b360e3
Eduardo <japabreu10@gmail.com>
2024-07-26 10:20:04 -0300

    prática

--- aula12/ex013.js
+++ aula12/ex013.js
@@ -16,7 +16,19 @@ switch(diaSem) {
         console.log('Terça-Feira')
         break
 
+    case 3: 
+        console.log('Quarta-Feira')
+        break
+
+    case 4: 
+        console.log('Quinta-Feira')
+        break
+
     case 5:
         console.log('Sexta-Feira')
+        break
+
+    case 6: 
+        console.log('Sábado')
         break
 }
---
+++ aula12/exercicios.js
@@ -0,0 +1,11 @@
+var idade = 19
+
+if (idade <= 10) {
+    console.log ('Olá amiguinho, tudo bem?')
+} else {
+    if (idade >10 && idade < 18) {
+        console.log ('Eae meu parceiro, beleza?')
+    } else {
+        console.log('Boa tarde, tudo bem?')
+    }
+}
