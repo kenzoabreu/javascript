9d3c7b353059a8361af19f76c4583284e2a0317b
Eduardo <japabreu10@gmail.com>
2024-08-07 12:49:57 -0300

    Update script.js

--- ex015/script.js
+++ ex015/script.js
@@ -8,6 +8,32 @@ function verificar() {
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number (fano.value)
-        res.innerHTML= `idade ${idade} `
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'homem'
+            if (idade >=0 && idade < 10) {
+                img.setAttribute('src', 'menino.jpg')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'jovem.jpg')
+            } else if (idade < 50) {
+
+            } else {
+
+            }
+        } else if (fsex[1].checked) {
+            genero = 'mulher'
+            if (idade >=0 && idade < 10) {
+
+            } else if (idade < 21) {
+
+            } else if (idade < 50) {
+
+            } else {
+                
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
     }
 }
