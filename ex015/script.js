652c4c23d6b40696d8fd0b47d66ccd33040d111b
Eduardo <japabreu10@gmail.com>
2024-08-07 19:38:54 -0300

    fim do projeto

--- ex015/script.js
+++ ex015/script.js
@@ -11,6 +11,8 @@ function verificar() {
         var genero = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
+        img.style.width = '150px' // Defina a largura desejada
+        img.style.height = '150px' // Defina a altura desejada
         if (fsex[0].checked) {
             genero = 'homem'
             if (idade >=0 && idade < 10) {
@@ -18,22 +20,23 @@ function verificar() {
             } else if (idade < 21) {
                 img.setAttribute('src', 'jovem.jpg')
             } else if (idade < 50) {
-
+                img.setAttribute('src', 'homem.jpg')
             } else {
-
+                img.setAttribute('src', 'idoso.jpg')
             }
         } else if (fsex[1].checked) {
             genero = 'mulher'
             if (idade >=0 && idade < 10) {
-
+                img.setAttribute('src', 'menina.jpg')
             } else if (idade < 21) {
-
+                img.setAttribute('src', 'jovemm.jpg')
             } else if (idade < 50) {
-
+                img.setAttribute('src', 'mulher.jpg')
             } else {
-                
+                img.setAttribute('src', 'idosa.jpg')
             }
         }
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
